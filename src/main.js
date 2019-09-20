a0d71a5d803f7017d798d9ed92c5bcaa5e833f61
nigel_leeeeee <nigalboa@gmail.com>
2019-09-20 15:30:36 +0800

    登入/登出，商品列表
    登入/登出，商品列表，路由控制

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
+axios.defaults.withCredentials = true
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +16,24 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
+})
+
+router.beforeEach((to, from, next) => {
+  console.log('to', to, 'from', from, 'next', next)
+  if (to.meta.requiresAuth) {
+    const api = `${process.env.APIPATH}/api/user/check`
+    // const vm = this
+    axios.post(api).then((response) => {
+      console.log(response.data)
+      if (response.data.success) {
+        next()
+      } else {
+        next({
+          path: '/login'
+        })
+      }
+    })
+  } else {
+    next()
+  }
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/pages/login'
+import Dashboard from '@/components/Dashboard'
+import Products from '@/components/pages/Products'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '*',
+      redirect: 'Login'
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/admin',
+      name: 'Dashboard',
+      component: Dashboard,
+      children: [
+        {
+          path: 'products',
+          name: 'Products',
+          component: Products,
+          meta: { requiresAuth: true }
+        }
+      ]
     }
   ]
 })
